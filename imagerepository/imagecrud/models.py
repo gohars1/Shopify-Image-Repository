d2d249754751b1aa9f8651a78aa01ddfa05402f2
unknown <gohars1@mcmaster.ca>
2020-12-30 20:19:19 -0500

    Extended Auth User and Added static file functionality

--- imagerepository/imagecrud/models.py
+++ imagerepository/imagecrud/models.py
@@ -1,20 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Image(models.Model):
-    name = models.CharField(max_length=200)
+    imgurl = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     stock = models.IntegerField(default=10)
 
 # class User(models.Model):
 #     wallet = models.IntegerField(default=200)
 #     # owned_images = models.ForeignKey(Image, on_delete=models.CASCADE, blank = True, null = True)
-# class UserInfo(models.Model):
-#     wallet = models.
 class UserInfo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     wallet = models.IntegerField(default=200)
 
 class UserImages(models.Model):
     image_id = models.ForeignKey(Image, on_delete=models.CASCADE, blank = True, null = True) 
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)   
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)
+
+@receiver(post_save, sender=User)
+def create_user_info(sender, instance, created, **kwargs):
+    if created:
+        UserInfo.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_info(sender, instance, **kwargs):
+    instance.userinfo.save()
--- imagerepository/imagecrud/urls.py
+++ imagerepository/imagecrud/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('buy/', views.buy, name='buy'),
     path('<int:image_id>/<int:user_id>/buy/', views.buy, name='buy'),
-]
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
