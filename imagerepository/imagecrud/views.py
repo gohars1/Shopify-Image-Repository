66e5be96ea97bbbb1db68256bb06516cd968367f
unknown <gohars1@mcmaster.ca>
2020-12-31 11:33:34 -0500

    Webpage has working buy and view images functionality

--- imagerepository/imagecrud/views.py
+++ imagerepository/imagecrud/views.py
@@ -17,42 +17,52 @@ def buy(request, image_id, user_id):
 
     image = Image.objects.get(id=image_id)
     user = User.objects.get(id=user_id).userinfo 
-    new_wallet = user.wallet - image.price
-    user.wallet = new_wallet
-    user.save()
-    new_stock = Image.objects.get(id=image_id).stock - 1
-    image.stock = new_stock
-    image.save()
-    user = User.objects.get(username=request.user.username)
-    if (UserImages.objects.filter(image_id=image,user_id=user).exists()):
-
-        userimage = UserImages.objects.get(image_id=image,user_id=user)
-        print(userimage.count)
-        new_count = userimage.count + 1
-        print(new_count)
-        userimage.count = new_count
-        print(userimage, userimage.count)
-        userimage.save()
+    if (user.wallet < image.price or image.stock == 0):
+        good_purchase = False
+        context = { 'user' : user, 'good_purchase' : good_purchase }
+        template = loader.get_template('imagecrud/buy.html')
+        return HttpResponse(template.render(context, request))
     else:
+        good_purchase = True
+        new_wallet = user.wallet - image.price
+        user.wallet = new_wallet
+        user.save()
+        new_stock = Image.objects.get(id=image_id).stock - 1
+        image.stock = new_stock
+        image.save()
+        user = User.objects.get(username=request.user.username)
+        if (UserImages.objects.filter(image_id=image,user_id=user).exists()):
 
-        owned_images = UserImages(image_id=image,user_id=user)
-        owned_images.save()
+            userimage = UserImages.objects.get(image_id=image,user_id=user)
+            new_count = userimage.count + 1
+            userimage.count = new_count
+            userimage.save()
+        else:
 
-    user = User.objects.get(username=request.user.username).userinfo
-    template = loader.get_template('imagecrud/buy.html')
-    images = Image.objects.all()
+            owned_images = UserImages(image_id=image,user_id=user)
+            owned_images.save()
 
-    context = { 'images' : images , 'user' : user }
+        user = User.objects.get(username=request.user.username).userinfo
+        template = loader.get_template('imagecrud/buy.html')
+        images = Image.objects.all()
 
-    return HttpResponse(template.render(context, request))
+        context = { 'good_purchase' : good_purchase, 'images' : images , 'user' : user }
+
+        return HttpResponse(template.render(context, request))
 
 def view_photos(request):
     user_id = User.objects.get(username=request.user.username).id
     user_images = UserImages.objects.filter(user_id=user_id)
+    
     images = []
     for i in user_images:
-        current_image = Image.objects.get(i.image_id)
         
+        current_image = Image.objects.get(id=i.image_id.id)
+        count = i.count
+        while (count > 0):
+            images.append(current_image)
+            count -=1
+
     template = loader.get_template('imagecrud/view_photos.html')
 
     context = { 'images' : images }
